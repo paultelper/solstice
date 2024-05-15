e1c0e77dcf47b024281095df4eaa527a3a1e2b01
ClarrieO5 <106720670+ClarrieO5@users.noreply.github.com>
2024-05-15 16:32:44 +1000

    Revert

--- app/src/index.js
+++ app/src/index.js
@@ -1,41 +1,35 @@
 import express from "express";
-import { createServer } from "http";
+import { createServer } from "node:http";
 import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux";
-import { join } from "path";
-import { hostname } from "os";
-import wisp from "wisp-server-node";
-import { createBareServer } from "@tomphttp/bare-server-node";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import wisp from "wisp-server-node"
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(join(__dirname, "static")));
-
-app.get('/app', (req, res) => {
-  res.sendFile(join(__dirname, './static/index.html'));
-});
-
+// Load our publicPath first and prioritize it over UV.
 app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
 
+// Error for everything else
 app.use((req, res) => {
-  res.status(404).sendFile(join(publicPath, "404.html"));
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
 });
 
-const server = createServer(app);
+const server = createServer();
 
 server.on("request", (req, res) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  app.handle(req, res);
+  app(req, res);
 });
-
 server.on("upgrade", (req, socket, head) => {
   if (req.url.endsWith("/wisp/"))
     wisp.routeRequest(req, socket, head);
@@ -49,21 +43,28 @@ if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
   const address = server.address();
-  console.log(`http://localhost:${address.port}`);
-  console.log(`http://${hostname()}:${address.port}`);
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
   console.log(
-    `http://${address.family === "IPv6" ? `[${address.address}]` : address.address
+    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
     }:${address.port}`
   );
 });
 
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
-  server.close(() => {
-    process.exit(0);
-  });
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  process.exit(0);
 }
 
-server.listen(port);
+server.listen({
+  port,
+});
